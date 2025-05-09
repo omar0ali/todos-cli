78babc1c4962d0fee8d3b27b3e6e85b56e8b099f
omar0ali <bajunaidomar@gmail.com>
2025-05-09 02:13:08 -0400

    Fixed the show command, it wasn't showing all the tasks by default. Improved documentation

--- cmd/show.go
+++ cmd/show.go
@@ -1,15 +1,15 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/omar0ali/todos/core"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var showCmd = &cobra.Command{
 	Use:   "show",
-	Short: "Showing the list of tasks",
-	Long:  "Showing a list of tasks in a table, which also includes the status of each task.",
+	Short: "Display the list of tasks",
+	Long:  "Displays all tasks in a formatted table, including the status of each task.",
 	Run: func(cmd *cobra.Command, args []string) {
 		verbose, _ := cmd.Flags().GetBool("verbose")
 		status, _ := cmd.Flags().GetString("status")
@@ -19,8 +19,8 @@ var showCmd = &cobra.Command{
 			if core.IsValidStatus(core.Status(status)) {
 				core.DisplayRecordsStatus(rows, verbose, core.Status(status))
 			} else {
-				fmt.Println("Unknown status:", status)
-				fmt.Println("Valid options: todo, in-progress, done")
+				log.Println("Unknown status:", status)
+				log.Println("Valid options: todo, in-progress, done")
 				core.DisplayRecords(rows, verbose)
 			}
 		}
@@ -30,5 +30,5 @@ var showCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(showCmd)
-	showCmd.Flags().StringP("status", "s", "todo", "Display a list of tasks based on its status")
+	showCmd.Flags().StringP("status", "s", "", "Display a list of tasks based on its status")
 }
