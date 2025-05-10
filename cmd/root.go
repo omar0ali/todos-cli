0bc70e5fed95c8bf08ea8284fce7c286d5e066bd
omar0ali <bajunaidomar@gmail.com>
2025-05-09 20:39:51 -0400

    Added version in root

--- cmd/root.go
+++ cmd/root.go
@@ -17,9 +17,10 @@ var (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "todos",
-	Short: "A simple task manager and to-do list tracker.",
-	Long:  "Todos is a CLI application that helps users manage their to-do tasks. You can add, remove, edit tasks, assign statuses, and display the latest tasks.",
+	Use:     "todos",
+	Short:   "A simple task manager and to-do list tracker.",
+	Long:    "Todos is a CLI application that helps users manage their to-do tasks. You can add, remove, edit tasks, assign statuses, and display the latest tasks.",
+	Version: "0.02",
 	Run: func(cmd *cobra.Command, args []string) {
 		verbose, _ := cmd.PersistentFlags().GetBool("verbose")
 		if verbose {
